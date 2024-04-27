77dd15b25464a84a1695e572352a8b9d63e9cc98
PaigeRussell <104575754+PaigeRU@users.noreply.github.com>
2024-04-27 21:12:46 +0200

    added home page and time log navigations

---
+++ app/src/main/java/com/example/part2poe/ui/addtimelog/AddTimeLogFragment.kt
@@ -0,0 +1,6 @@
+package com.example.part2poe.ui.addtimelog
+
+import androidx.fragment.app.Fragment
+
+class AddTimeLogFragment: Fragment() {
+}
--- app/src/main/java/com/example/part2poe/ui/maintimelog/MainTimeLogFragment.kt
+++ app/src/main/java/com/example/part2poe/ui/maintimelog/MainTimeLogFragment.kt
@@ -1,6 +1,30 @@
 package com.example.part2poe.ui.maintimelog
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.part2poe.R
+import com.example.part2poe.ui.register.RegisterFragmentDirections
 
 class MainTimeLogFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_maintimelog, container, false)
+
+        val btnAddTimeLog: Button = view.findViewById(R.id.btnAddTimeLog)
+        btnAddTimeLog.setOnClickListener {
+
+            findNavController().navigate(MainTimeLogFragmentDirections.actionMaintimelogFragmentToAddtimelogFragment())
+        }
+
+        return view
+    }
+
 }
