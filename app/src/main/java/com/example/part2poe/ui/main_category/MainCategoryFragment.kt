e2d50b8c9e94c1c2ae0cfbfd2ac3c5227ac657e9
nikkigordhan <ST10108243@vcconnect.edu.za>
2024-04-28 12:07:54 +0200

    Added the maincategory

---
+++ app/src/main/java/com/example/part2poe/ui/main_category/MainCategoryFragment.kt
@@ -0,0 +1,45 @@
+package com.example.part2poe.ui.main_category
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.part2poe.R
+import com.example.part2poe.databinding.FragmentMainCategoryBinding
+
+class MainCategoryFragment : Fragment() {
+    private var _binding: FragmentMainCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainCategoryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        // Find the Add New Category button and set a click listener
+        val addCategoryButton: Button = binding.btnAddNewCategory
+        addCategoryButton.setOnClickListener {
+           // navigateToAddCategory()
+        }
+
+        //categroyArray()
+
+        return root
+    }
+
+    /*private fun navigateToAddCategory() {
+        findNavController().navigate(MainCategoryFragmentDirections.actionMaincategoryFragmentToAddcategroyFragment())
+    }*/
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
