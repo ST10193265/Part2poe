606d1749c08367601a1a7c170d0658f7edf06807
nikkigordhan <ST10108243@vcconnect.edu.za>
2024-04-29 19:36:24 +0200

    try to display the category on the main screen

--- app/src/main/java/com/example/part2poe/ui/main_category/MainCategoryFragment.kt
+++ app/src/main/java/com/example/part2poe/ui/main_category/MainCategoryFragment.kt
@@ -1,15 +1,17 @@
-package com.example.part2poe.ui.main_category
-
-import androidx.fragment.app.viewModels
-import android.os.Bundle
+import android.R
 import androidx.fragment.app.Fragment
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
-import com.example.part2poe.R
+
 import com.example.part2poe.databinding.FragmentMainCategoryBinding
+import com.example.part2poe.ui.GlobalVar
+import com.example.part2poe.ui.main_category.MainCategoryFragmentDirections
 
 class MainCategoryFragment : Fragment() {
     private var _binding: FragmentMainCategoryBinding? = null
@@ -23,12 +25,21 @@ class MainCategoryFragment : Fragment() {
         _binding = FragmentMainCategoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val testlist = binding.testlist
+
+        // Create a list of category names
+        val categoryNames = GlobalVar.GlobalVariables.oagCategory.map { it.categoryname }
+
+        // Create an adapter using the category names and a layout for list items
+        val adapter = ArrayAdapter(requireContext(), R.layout.simple_list_item_1, categoryNames)
+
+       // testlist.adapter = adapter
+
         // Find the Add New Category button and set a click listener
         val addCategoryButton: Button = binding.btnAddNewCategory
         addCategoryButton.setOnClickListener {
-           navigateToAddCategory()
+            navigateToAddCategory()
         }
-
 
         return root
     }
